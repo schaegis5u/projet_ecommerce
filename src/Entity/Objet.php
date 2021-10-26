da9a95110e692ea9bfeeb38133c40594fe01763f
Aurélien <schaegis5u@etu.univ-lorraine.fr>
2021-10-26 12:26:17 +0200

    Ajout d'une entité : Objet
    Objet correspond à ce qui est mis en vente. Contient ID, nom, description, marque, prix

---
+++ src/Entity/Objet.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ObjetRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ObjetRepository::class)
+ */
+class Objet
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Nom;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $Description;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $Marque;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $Prix;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->Nom;
+    }
+
+    public function setNom(string $Nom): self
+    {
+        $this->Nom = $Nom;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->Description;
+    }
+
+    public function setDescription(?string $Description): self
+    {
+        $this->Description = $Description;
+
+        return $this;
+    }
+
+    public function getMarque(): ?string
+    {
+        return $this->Marque;
+    }
+
+    public function setMarque(?string $Marque): self
+    {
+        $this->Marque = $Marque;
+
+        return $this;
+    }
+
+    public function getPrix(): ?float
+    {
+        return $this->Prix;
+    }
+
+    public function setPrix(float $Prix): self
+    {
+        $this->Prix = $Prix;
+
+        return $this;
+    }
+}
