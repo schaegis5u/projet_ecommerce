0b224bd8ffd08ea07dbf36054e95199333dfd9e0
Aurélien <schaegis5u@etu.univ-lorraine.fr>
2021-12-20 18:36:10 +0100

    retrait (temporaire ?) de la page 0, inutile

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoriesRepository;
+use App\Repository\ObjetRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class AppController extends AbstractController
 {
     #[Route('/', name: 'app')]
-    public function index(): Response
+    public function index(ObjetRepository $objetRepository, CategoriesRepository $categoriesRepository): Response
     {
-        return $this->render('app/index.html.twig', [
+        /**return $this->render('app/index.html.twig', [
             'controller_name' => 'AppController',
+        ]);**/
+        return $this->render('objet/index.html.twig', [
+            'objets' => $objetRepository->findAll(),
+            'categoryList' => $categoriesRepository->findAll(),
+
         ]);
     }
 }
